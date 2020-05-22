1fdc52f0ff35a08fe65945b6efa16671e79cb33d
pcapriles <pedro.capriles@walmart.com>
2020-05-22 00:05:17 -0400

    Add actions, reducers, types and integration web api for the example, install react redux and redux persist

---
+++ src/components/Loading/index.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+const Loading = ({message}) => {
+    return(
+        <Fragment>
+            <h1>
+                Loading...
+            </h1>
+        </Fragment>
+    )
+}
+
+export default Loading;
